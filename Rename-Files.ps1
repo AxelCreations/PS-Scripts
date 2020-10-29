cdcac46e64b0dab3cbb694226e5903c13ac9d1ad
AxelCreations <30634177+AxelCreations@users.noreply.github.com>
2020-10-29 10:47:36 -0400

    Added renaming files Scripts

---
+++ Rename-Files.ps1
@@ -0,0 +1,14 @@
+﻿$FilesPath = "D:\FilesPath\";
+$Files = Get-ChildItem -Path $FilesPath -Filter *.mp4 | Select-Object Name
+$FilesNames = Get-Content("D:\FilesPath\FilesNames.txt");
+$number = 1;
+
+foreach($name in $FilesNames)
+{
+    $oldName = ($FilesPath + "lesson$number" + ".mp4");
+    $newName = ($FilesPath + "$number - " + $name.Trim() + ".mp4");
+
+    Rename-Item -Path $oldName -NewName $newName
+
+    $number++;
+}
